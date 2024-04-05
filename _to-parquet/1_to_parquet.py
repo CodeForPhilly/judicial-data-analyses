e19af6933455ca6588efe60c2b77a26f35953ddc
Michael Chow <mc_al_github@fastmail.com>
2024-04-05 17:38:25 -0400

    update to-parquet

--- _to-parquet/1_to_parquet.py
+++ _to-parquet/1_to_parquet.py
@@ -1,5 +1,6 @@
 #%%
 import polars as pl
+import polars.selectors as cs
 from pathlib import Path
 
 data_paths = Path("../_data").glob("*.txt")
@@ -7,16 +8,27 @@ Path("out").mkdir(exist_ok=True)
 
 for path in data_paths:
     print(path)
-    if "CPCMS_SentenceData" in path.name:
-        # note that for some reason, both polars and pandas CSV
-        # readers believe that some lines in this file have 26 fields,
-        # when they should have 25. I looked at one manually (1964815)
-        # and couldn't see anything wrong with it.
+
+    if "Alias" in path.name:
         continue
 
+    # note that I deleted an unescaped | in a text field
+    # on line 1965440 of CPCMS_SentenceData
     df = pl.read_csv(path, separator="|", infer_schema_length=100_000_000_000)
 
-    df.write_parquet(f"out/{path.with_suffix('.parquet').name}")
-
+    if "CaseData" in path.name:
+        # hash defendant name and date of birth
+        res = (
+            df
+            .with_columns(
+                DefendantID=(pl.col("DefendantName") + "---" + pl.col("DefendantDOB")).hash(),
+                DefendantDOB=pl.col("DefendantDOB").str.to_date().dt.truncate("1mo")
+            )
+            .select(cs.all().exclude("DefendantName"))
+        )
+    else:
+        res = df
+ 
+    res.write_parquet(f"out/{path.with_suffix('.parquet').name}")
 
 # %%
